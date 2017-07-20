edb4ae6e3060a28b638a763ab6282e134987dfe9
Aaron Starkey <aaronstarkey@gmail.com>
2017-07-20 10:27:05 -0700

    move header below image

--- wp-content/themes/understrap-child-master/loop-templates/content-single.php
+++ wp-content/themes/understrap-child-master/loop-templates/content-single.php
@@ -8,7 +8,10 @@
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
+	<div class="single-post-image">
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	</div>
+		<header class="entry-header">
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
@@ -21,9 +24,6 @@
 		<?php the_category( ' | ' ); ?>
 
 	</header><!-- .entry-header -->
-	<div class="single-post-image">
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-	</div>
 	<div class="entry-content">
 		<?php the_content(); ?>
 
