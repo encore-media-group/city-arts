860988f41b67acfeb51a24dd20f3247a3e017acd
Aaron Starkey <aaronstarkey@gmail.com>
2017-07-21 09:09:04 -0700

    split article page into two columns

--- wp-content/themes/understrap-child-master/loop-templates/content-single.php
+++ wp-content/themes/understrap-child-master/loop-templates/content-single.php
@@ -10,27 +10,19 @@
 $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "full" );
 $thumbnail_url = $thumbnail[0];
 ?>
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class('row'); ?> id="post-<?php the_ID(); ?>">
 
-	<div class="single-post-image-hero" style="background-image: url('<?php echo $thumbnail_url; ?>');">
-	<?php //echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	<div class="single-post-image-hero col-lg-6" style="background-image: url('<?php echo $thumbnail_url; ?>');">
+
 	</div>
-		<header class="entry-header">
-
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-
-			<?php understrap_posted_on(); ?>
-
-
-		</div><!-- .entry-meta -->
-		<?php the_category( ' | ' ); ?>
-
-	</header><!-- .entry-header -->
-	<div class="entry-content">
+	<div class="col-lg-6">
+			<header class="entry-header row">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					<div class="entry-meta"><?php understrap_posted_on(); ?></div><!-- .entry-meta -->
+					<?php the_category( ' | ' ); ?>
+				</header><!-- .entry-header -->
+	<div class="entry-content row">
 		<?php the_content(); ?>
-
 		<div class="attached-images">
 			<ul>
 			<?php
@@ -49,7 +41,7 @@ $thumbnail_url = $thumbnail[0];
 		?>
 
 	</div><!-- .entry-content -->
-
+	</div><!--end col-lg-6-->
 	<footer class="entry-footer">
 
 		<?php understrap_entry_footer(); ?>
