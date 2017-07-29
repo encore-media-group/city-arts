8ad24af0b1864be538f7bb82dc0b9e369f8825fc
Aaron Starkey <aaronstarkey@gmail.com>
2017-07-29 18:54:28 -0500

    update slug filter to exclude ( and )

--- wp-content/plugins/caimportplugin/caimportplugin.php
+++ wp-content/plugins/caimportplugin/caimportplugin.php
@@ -802,6 +802,8 @@ function slug($string, $length = -1, $separator = '-') {
   //not for ca $string = strtolower($string);
 
   // replace non alphanumeric and non underscore charachters by separator
+  $string = preg_replace('/[(|)]/i', '', $string);
+
   $string = preg_replace('/[^a-z0-9\._]/i', $separator, $string);
 
   // replace multiple occurences of separator by one instance
