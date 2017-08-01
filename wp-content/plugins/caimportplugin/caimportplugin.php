fa9c61bfd27a3685e7236970a60593059870883b
Aaron Starkey <aaronstarkey@gmail.com>
2017-07-31 21:46:17 -0500

    convert + to - in slug funtion

--- wp-content/plugins/caimportplugin/caimportplugin.php
+++ wp-content/plugins/caimportplugin/caimportplugin.php
@@ -810,6 +810,8 @@ function slug($string, $length = -1, $separator = '-') {
 
   // replace non alphanumeric and non underscore charachters by separator
   $string = str_replace(".JPG", ".jpg", $string);
+  $string = str_replace("\+", "-", $string);
+
   $string = preg_replace('/[(|)|\[|\]]/i', '', $string);
 
   $string = preg_replace('/[^a-z0-9\._]/i', $separator, $string);
