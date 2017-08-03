fadc1cb0207aeb8c036e6b6d49f687597485a651
Aaron Starkey <aaronstarkey@gmail.com>
2017-08-02 20:03:08 -0700

    update acf plugin and footer.php

--- wp-content/themes/understrap-child-master/footer.php
+++ wp-content/themes/understrap-child-master/footer.php
@@ -16,6 +16,20 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="wrapper" id="wrapper-footer">
 
 	<div class="<?php echo esc_attr( $container ); ?>">
+		<div class="row">
+			<div class="col-md-3">
+				<h5>NEWSLETTERS</h5>
+			</div>
+			<div class="col-md-3">
+				<h5>MAGAZINE</h5>
+			</div>
+			<div class="col-md-3">
+				<h5>ABOUT</h5>
+			</div>
+			<div class="col-md-3">
+				<h5>SOCIAL</h5>
+			</div>
+		</div>
 
 		<div class="row">
 
