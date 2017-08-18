4ec2eb879183ae61cdf54ccc9547b8a872c5d479
Aaron Starkey <aaronstarkey@gmail.com>
2017-08-18 10:07:40 -0700

    refactor style sheet and add archive page

---
+++ wp-content/themes/understrap-child-master/archive.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package understrap
+ */
+
+get_header();
+?>
+
+<?php
+$container   = get_theme_mod( 'understrap_container_type' );
+$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
+?>
+
+<div class="wrapper" id="archive-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<main class="col site-main" id="main">
+
+				<?php if ( have_posts() ) : ?>
+
+					<header class="page-header">
+						<h3 class="page-title sidelines sidebar py-4"> <?php single_cat_title() ?> </h3>
+					<?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
+					</header><!-- .page-header -->
+
+					<div class="row">
+
+					<?php while ( have_posts() ) : the_post(); ?>
+						<div class="col-3">
+						<?php
+						 get_template_part( 'item-templates/item', 'medium-small' );
+						?>
+						</div>
+					<?php endwhile; ?>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+
+				<?php endif; ?>
+
+				</div><!--row-->
+			</main><!-- #main -->
+
+		</div><!-- #primary -->
+
+	</div> <!-- .row -->
+
+</div><!-- Container end -->
+
+<divi class="container">
+	<div class="row justify-content-center">
+		<div class="col-auto">
+		<!-- The pagination component -->
+		<?php understrap_pagination(); ?>
+		</div>
+	</div>
+</divi>
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
--- wp-content/themes/understrap-child-master/item-templates/item-medium-small.php
+++ wp-content/themes/understrap-child-master/item-templates/item-medium-small.php
@@ -10,8 +10,8 @@
   $thumbnail_id = get_post_thumbnail_id( $post->ID );
   $thumbnail_caption = get_post($thumbnail_id)->post_excerpt;
 
-  $img_src = wp_get_attachment_image_url( $thumbnail_id, 'medium' );
-  $img_srcset = wp_get_attachment_image_srcset( $thumbnail_id, 'medium' );
+  $img_src = wp_get_attachment_image_url( $thumbnail_id, 'full' );
+  $img_srcset = wp_get_attachment_image_srcset( $thumbnail_id, 'full' );
 ?>
   <!-- medium small -->
   <div class="row item-medium-small">
@@ -19,8 +19,8 @@
       <img
        src="<?php echo esc_url( $img_src ); ?>"
        srcset="<?php echo esc_attr( $img_srcset ); ?>"
-       sizes="(max-width: 255px) 100vw, 255px"
-       style="height: 191px;"
+       sizes="(max-width: 2000px) 100vw, 255px"
+       style="height: 170px;"
        class="img-fluid"
        alt="">
     </div>
