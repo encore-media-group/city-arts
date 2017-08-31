94c5b812d7eb36742b75ab62da59a158c3292846
Aaron Starkey <aaronstarkey@gmail.com>
2017-08-30 19:59:51 -0700

    big search update and addition of relevanssi

--- wp-content/themes/understrap-child-master/search.php
+++ wp-content/themes/understrap-child-master/search.php
@@ -13,6 +13,19 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 
 <div class="wrapper" id="search-wrapper">
 
+	<div class="container">
+		<div class="row mb-4">
+			<div class="col">
+				<?php echo get_search_form(); ?>
+			</div>
+		</div>
+		<header class="row page-header">
+			<h1 class="col text-center page-title sidelines sidebar"><?php printf(
+				esc_html__( 'Search Results for: %s', 'understrap' ),
+			'<span>' . get_search_query() . '</span>' ); ?></h1>
+		</header><!-- .page-header -->
+	</div>
+
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
@@ -21,17 +34,11 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 			<?php get_template_part( 'global-templates/left-sidebar-check', 'none' ); ?>
 
 			<main class="site-main" id="main">
-
 				<?php if ( have_posts() ) : ?>
 
-					<header class="page-header">
 
-							<h1 class="page-title"><?php printf(
-							/* translators:*/
-							 esc_html__( 'Search Results for: %s', 'understrap' ),
-								'<span>' . get_search_query() . '</span>' ); ?></h1>
 
-					</header><!-- .page-header -->
+
 
 					<?php /* Start the Loop */ ?>
 					<?php while ( have_posts() ) : the_post(); ?>
