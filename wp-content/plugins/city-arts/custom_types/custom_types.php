3a356b39d7d507c3662aadd3b52c4c4566c2382a
Aaron Starkey <aaronstarkey@gmail.com>
2017-09-27 14:28:30 -0700

    add secondary image custom type

--- wp-content/plugins/city-arts/custom_types/custom_types.php
+++ wp-content/plugins/city-arts/custom_types/custom_types.php
@@ -259,6 +259,58 @@ acf_add_local_field_group(array (
   'description' => '',
 ));
 
+
+
+
+acf_add_local_field_group(array (
+  'key' => 'group_59cbf002eb226',
+  'title' => 'Article Enchanced',
+  'fields' => array (
+    array (
+      'key' => 'field_59cbf02b11265',
+      'label' => 'Secondary Feature Image',
+      'name' => 'secondary_feature_image',
+      'type' => 'image',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'return_format' => 'array',
+      'preview_size' => 'ca-540x360',
+      'library' => 'all',
+      'min_width' => 540,
+      'min_height' => '',
+      'min_size' => '',
+      'max_width' => '',
+      'max_height' => '',
+      'max_size' => '',
+      'mime_types' => '',
+    ),
+  ),
+  'location' => array (
+    array (
+      array (
+        'param' => 'post_taxonomy',
+        'operator' => '==',
+        'value' => 'article_format:article-enhanced',
+      ),
+    ),
+  ),
+  'menu_order' => 0,
+  'position' => 'normal',
+  'style' => 'default',
+  'label_placement' => 'top',
+  'instruction_placement' => 'label',
+  'hide_on_screen' => '',
+  'active' => 1,
+  'description' => '',
+));
+
+
   endif;
 
 
