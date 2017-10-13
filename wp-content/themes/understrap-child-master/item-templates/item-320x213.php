861b30b52a926b5cabe827d656198629ca079562
aaronstarkey <aaronstarkey@gmail.com>
2017-10-13 00:24:35 -0700

    another test using 320x

--- wp-content/themes/understrap-child-master/item-templates/item-320x213.php
+++ wp-content/themes/understrap-child-master/item-templates/item-320x213.php
@@ -13,6 +13,31 @@
   $img_src = wp_get_attachment_image_url( $thumbnail_id, 'ca-320x213' );
   $img_srcset = wp_get_attachment_image_srcset( $thumbnail_id, 'ca-320x213' );
 
+  $image_attachment_metadata = wp_get_attachment_metadata($thumbnail_id);
+  $img_width = isset($image_attachment_metadata['width']) ? $image_attachment_metadata['width'] : 0;
+  $img_height = isset($image_attachment_metadata['height']) ? $image_attachment_metadata['height'] : 0;
+
+  $img_orientation = 'landscape';
+
+if( $img_width < $img_height ) {
+  $img_orientation = 'portrait';
+  $img_src = wp_get_attachment_image_url( $thumbnail_id, 'ca-320x426' );
+  $img_srcset = wp_get_attachment_image_srcset( $thumbnail_id, 'ca-320x426' );
+} else {
+  $img_src = wp_get_attachment_image_url( $thumbnail_id, 'ca-320x213' );
+  $img_srcset = wp_get_attachment_image_srcset( $thumbnail_id, 'ca-320x213' );
+}
+
+
+
+
+
+
+
+
+
+
+
   $item_css = isset($item_css) ? $item_css : ' col-12 col-sm ';
 
   $show_byline = isset($show_byline) ? $show_byline : false;
@@ -22,7 +47,7 @@
 
   <div class="<? echo $item_css ?> item-320x213">
     <div class="row">
-      <div class="col-12 col-lg">
+      <div class="col-12 col-lg" style="overflow: hidden;max-height: 320px;">
         <img src="<?php echo esc_url( $img_src ); ?>"
          srcset="<?php echo esc_attr( $img_srcset ); ?>"
          sizes="(max-width: 46em) 100vw, 320px"
