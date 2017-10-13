c5ba538a3d4806c0da5942459fc54cf21fef574a
aaronstarkey <aaronstarkey@gmail.com>
2017-10-13 00:51:56 -0700

    add grey bar to archive landscape ad

--- wp-content/themes/understrap-child-master/archive.php
+++ wp-content/themes/understrap-child-master/archive.php
@@ -11,8 +11,6 @@ get_header();
 ?>
 
 <?php
-$container   = get_theme_mod( 'understrap_container_type' );
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 
 $archive_slug =  get_queried_object()->slug;
 
@@ -57,92 +55,98 @@ $the_query = new WP_Query(array(
 ?>
 
 <div class="wrapper <?= $is_discipline_archive_class ?> " id="archive-wrapper">
-
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
-		<div class="row pt-4">
-
-			<main class="col site-main" id="main">
-				<?php
-				if ( have_posts() ) : ?>
-						<!-- .page-header -->
-						<?php if ( $paged < 2 && $is_discipline_archive ) : ?>
-							<header class="page-header row">
-								<h1 class=" col page-title discipline-title text-center pb-3"> <?php single_cat_title() ?> </h1>
-							</header>
-
-						<?php elseif ( $paged < 2 && !$is_discipline_archive ) : ?>
-							<header class="page-header row">
-								<div class="col-12">
-									<h2 class="page-title pb-4 sidelines"> <?php single_cat_title() ?> </h2>
-								</div>
-								<div class="col-12">
-									<div class="row justify-content-center">
-										<?php the_archive_description( '<div class="col-12 col-lg-10 pb-4 taxonomy-description">', '</div>' ); ?>
-									</div>
-								</div>
-						</header><!-- .page-header -->
-						<?php else: ?> <!-- page greater than 2 -->
-							<header class="page-header row w-md-75 mx-auto">
-								<h2 class="col page-title pb-4 sidelines"> <?php single_cat_title() ?> </h2>
-							</header>
-						<?php endif; ?>
-
+	<main  class="site-main" id="main">
+		<div class="container" id="content" tabindex="-1">
+			<div class="row pt-4">
+				<div class="col">
 					<?php
-						$count = 1;
-					  echo '<div class="row">';
-            while( $the_query->have_posts() ) : $the_query->the_post();
-            	if( $is_discipline_archive && $paged == 1 ) :
-								if ( $count == 1 ) :
-						 			get_template_part( 'item-templates/item', '730x487-horizontal' );
-						 			echo '</div><!-- end row --><div class="row pt-4">';
-								elseif ($count == 2  ):
-						 			get_template_part( 'item-templates/item', '320x213' );
-								elseif ($count == 3  ):
-						 			get_template_part( 'item-templates/item', '320x213' );
-						 			echo '</div><!-- end row -->';
-				 					echo ad_728xlandscape_shortcode();
-						 			echo '<div class="row">';
-						 		elseif( $count <= 15 ): ?>
+					if ( have_posts() ) : ?>
+							<!-- .page-header -->
+							<?php if ( $paged < 2 && $is_discipline_archive ) : ?>
+								<header class="page-header row">
+									<h1 class=" col page-title discipline-title text-center pb-3"> <?php single_cat_title() ?> </h1>
+								</header>
+
+							<?php elseif ( $paged < 2 && !$is_discipline_archive ) : ?>
+								<header class="page-header row">
+									<div class="col-12">
+										<h2 class="page-title pb-4 sidelines"> <?php single_cat_title() ?> </h2>
+									</div>
+									<div class="col-12">
+										<div class="row justify-content-center">
+											<?php the_archive_description( '<div class="col-12 col-lg-10 pb-4 taxonomy-description">', '</div>' ); ?>
+										</div>
+									</div>
+							</header><!-- .page-header -->
+							<?php else: ?> <!-- page greater than 2 -->
+								<header class="page-header row w-md-75 mx-auto">
+									<h2 class="col page-title pb-4 sidelines"> <?php single_cat_title() ?> </h2>
+								</header>
+							<?php endif; ?>
+
+						<?php
+							$count = 1;
+						  echo '<div class="row">';
+	            while( $the_query->have_posts() ) : $the_query->the_post();
+	            	if( $is_discipline_archive && $paged == 1 ) :
+									if ( $count == 1 ) :
+							 			get_template_part( 'item-templates/item', '730x487-horizontal' );
+							 			echo '</div><!-- end row --><div class="row pt-4">';
+									elseif ($count == 2  ):
+							 			get_template_part( 'item-templates/item', '320x213' );
+									elseif ($count == 3  ):
+							 			get_template_part( 'item-templates/item', '320x213' );
+							 			echo '</div><!-- end row -->';
+							 	?>
+							 			</div><!--col-->
+							 		</div><!--row-->
+							 	</div><!--container-->
+							 	<div class="container-fluid ad-container">
+					 					<?php echo ad_728xlandscape_shortcode(); ?>
+					 			</div>
+							 	<div class="container">
+							 		<div class="row pt-4">
+										<div class="col">
+
+					 			<?php
+							 			echo '<div class="row">';
+							 		elseif( $count <= 15 ): ?>
+										<div class="col-12 col-sm-6 col-lg-3">
+											<?php get_template_part( 'item-templates/item', '255x170' ); ?>
+										</div>
+										<?php if ($count >= 4 && $count % 4 == 0) : echo '<div class="m-100"></div>'; endif; ?>
+							<?php
+	            		endif;
+								else:
+							?>
 									<div class="col-12 col-sm-6 col-lg-3">
 										<?php get_template_part( 'item-templates/item', '255x170' ); ?>
 									</div>
 									<?php if ($count >= 4 && $count % 4 == 0) : echo '<div class="m-100"></div>'; endif; ?>
-						<?php
-            		endif;
-							else:
+							<?php
+								endif;
+
+								$count++;
+							endwhile;
+							wp_reset_postdata();
+						endif;
 						?>
-								<div class="col-12 col-sm-6 col-lg-3">
-									<?php get_template_part( 'item-templates/item', '255x170' ); ?>
-								</div>
-								<?php if ($count >= 4 && $count % 4 == 0) : echo '<div class="m-100"></div>'; endif; ?>
-						<?php
-							endif;
 
-							$count++;
-						endwhile;
-						wp_reset_postdata();
-					endif;
-					?>
-
-				</div><!--row-->
-			</main><!-- #main -->
-
-		</div> <!-- .row -->
-
-	</div><!-- Container end -->
-
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-auto">
-			<!-- The pagination component -->
-			<?php
-			understrap_pagination( $the_query );
-			?>
+					</div><!--row-->
+				</div>
+			</div> <!-- .row -->
+		</div><!-- container end -->
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-auto">
+				<!-- The pagination component -->
+				<?php
+				understrap_pagination( $the_query );
+				?>
+				</div>
 			</div>
-		</div>
-	</div>
-
+		</div><!-- container end -->
+	</main><!-- #main -->
 </div><!-- Wrapper end -->
 
 <?php get_footer(); ?>
