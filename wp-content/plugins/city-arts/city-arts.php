6cb16b59b5b376b9c92872431c57aa2662e84d4d
aaronstarkey <aaronstarkey@gmail.com>
2017-10-18 02:20:28 -0700

    oops, typo. time to go to bed

--- wp-content/plugins/city-arts/city-arts.php
+++ wp-content/plugins/city-arts/city-arts.php
@@ -66,7 +66,7 @@ add_shortcode( 'insert_secondary_feature_image', 'secondary_feature_image_shortc
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' )
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 function ca_enqueue_scripts_and_styles() {
 }
