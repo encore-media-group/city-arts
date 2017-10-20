436d983d7f40bc542954a90651538ca9d76b729f
aaronstarkey <aaronstarkey@gmail.com>
2017-10-19 23:52:08 -0700

    move tracking code within header

--- wp-content/themes/understrap-child-master/header.php
+++ wp-content/themes/understrap-child-master/header.php
@@ -15,16 +15,6 @@ $current_cover = get_current_issue_image();
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-3120102-3"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
-
-	  gtag('config', 'UA-3120102-3');
-	</script>
-	<!--end GA -->
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -131,6 +121,15 @@ $current_cover = get_current_issue_image();
 
     });
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-3120102-3"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-3120102-3');
+	</script>
+	<!--end GA -->
 </head>
 <body <?php body_class(); ?>>
 
