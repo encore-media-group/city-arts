7b9ad0c61f9e1af17c2e5f6ac3a975f983564a93
aaronstarkey <aaronstarkey@gmail.com>
2018-04-25 00:52:27 -0700

    updates to campaign message

--- wp-content/themes/understrap-child-master/item-templates/content-promo.php
+++ wp-content/themes/understrap-child-master/item-templates/content-promo.php
@@ -1,7 +1,7 @@
 <div class="container my-4">
   <div class="row">
     <div class="col-12 py-3 promo">
-      <div class="float-left pr-2">The NEW City Arts is coming!</div><div class="float-left"><a style="text-decoration:underline;" href="https://igg.me/at/theNEWcityarts">Learn more and become a Founding Member today.</a></div>
+      <div class="float-left pr-2">We need your help! City Arts is becoming independent&mdash;and we need your support to make the transition. Join The NEW City Arts as a Founding Member today to secure the next chapter of the magazine you love! &raquo; <a class="learn-more" style="" href="https://igg.me/at/theNEWcityarts">LEARN MORE</a></div>
     </div>
   </div>
 </div>
