6b21765f39a80aa77b551f385e5149529a7ac156
aaronstarkey <aaronstarkey@gmail.com>
2018-05-03 12:57:56 -0700

    add logic to core label function to show purple label anytime a calendar post is displayed

--- wp-content/themes/understrap-child-master/item-templates/item-category-label.php
+++ wp-content/themes/understrap-child-master/item-templates/item-category-label.php
@@ -1,9 +1,26 @@
-
 <?php
 
 $cat_label = get_category_label();
+$cat_url = $cat_label['url'];
+$cat_name = $cat_label['name'];
 
-$yellow_label = '<a class="url fn n" href="%1$s"><span class="category-label align-middle text-center mb-2 px-2 py-1">%2$s</span></a>';
+$fields = get_fields( $post->ID);
+$calendar_label_class = "";
+$add_url = true;
 
-echo sprintf( $yellow_label, $cat_label['url'], $cat_label['name'] );
+if( !empty( $fields['show_in_calendar'] ) ) :
+	if( $fields['show_in_calendar'] == 'yes' ) :
+		$calendar_label_class = " calendar-item calendar-item-label ";
+		$add_url = false;
+	endif;
+endif;
+
+$span = sprintf( '<span class="category-label align-middle text-center mb-2 px-2 py-1 %2$s ">%1$s</span></a>' , $cat_name, $calendar_label_class );
+
+if( $add_url ) :
+	echo sprintf('<a class="url fn n" href="%1$s">%2$s</a>', $cat_url, $span );
+else:
+	echo $span;
+endif;
+
 ?>
