3b3dc5f24cab8b7ccc158c530e9bc2777fbff06d
Aaron Starkey <aaronstarkey@gmail.com>
2020-02-21 22:50:14 -0800

    remove subscribe box

--- wp-content/themes/understrap-child-master/item-templates/item-mailchimp.php
+++ wp-content/themes/understrap-child-master/item-templates/item-mailchimp.php
@@ -1,22 +1,7 @@
 
 <div class="row">
     <div class="col pb-4 mt-4">
-        <div class="mail-wrapper px-4 pb-4">
-            <h2 class="sidebar">Subscribe to our Newsletter</h2>
-            <h6 class="text-center current-text">Sign up and receive the best of City Arts in your inbox.</h6>
-                    <!-- Begin MailChimp Signup Form -->
-                <div id="mc_embed_signup">
-                    <form action="//encoremediagroup.us4.list-manage.com/subscribe/post?u=21f8fede41bd5e2c87aa9504b&amp;id=7d8b6778a7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate text-center my-2" target="_blank" novalidate>
-                        <div id="mc_embed_signup_scroll" >
-                            <input type="email" value="" name="EMAIL" class="email mb-2" id="mce-EMAIL" placeholder="email address" required>
-                            <input type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="button">
-                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_21f8fede41bd5e2c87aa9504b_7d8b6778a7" tabindex="-1" value=""></div>
-                        </div>
-                    </form>
-                </div><!--End mc_embed_signup-->
-            <h6 class="text-center current-text pt-2 mb-0">City Arts memberships are available <a href="/membership" style="text-decoration: underline;">here</a>.</h6>
-        </div>
+        
     </div>
 </div>
 
